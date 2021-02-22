bb6c8d9fe4e2b29a13eb338ff7ccd0ae47af5c31
Bruno Seidi Koura <65494771+BrunoSeidi@users.noreply.github.com>
2021-02-22 07:59:59 -0300

    Update week 1.py

--- week 1.py
+++ week 1.py
@@ -3,3 +3,20 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the '+' sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+
+## 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year % 4 == 0:
+  if year % 100 ==0:
+    if year % 400 == 0:
+       print("leap year")
+    else:
+      print("not leap year")
+  else:
+    print("leap year")
+else:
+  ("not")
