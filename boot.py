228ec3a69d6cc552da0bc11e65eab09f5df651aa
Bruno Seidi Koura <65494771+BrunoSeidi@users.noreply.github.com>
2021-04-06 13:18:15 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -7,9 +7,20 @@ if number % 2 == 0:
 else:
   print(f"its odd")
 
-  
+  /////////////////
   
   import random
 
 coin= ["head", "tails", "bruno", "hello"]
 print(random.choice(coin))
+
+////////////////////
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random 
+
+whoWillPay = random.choice(names)
+print(whoWillPay)
